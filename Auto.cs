ebf6e6de8acc19d0b8c3aad5fe1e623985b2c1ff
серожа <cepbora@vk.com>
2023-10-03 16:58:41 +0300

    Добавьте файлы проекта.

---
+++ Auto.cs
@@ -0,0 +1,118 @@
+﻿using System;
+namespace bebra
+{
+    class auto
+    {
+        private string nom;
+        private double bak;
+        private double ras;
+        private double ost;
+        private int speed;
+        private double probeg;
+
+        public auto(double initialFuel, int initialSpeed)
+        {
+            this.bak = initialFuel;
+            this.speed = initialSpeed;
+        }
+
+        public void Info(string nom, double bak, double ras, int speed, double probeg)
+        {
+            this.nom = nom;
+            this.bak = bak;
+            this.ras = ras;
+            this.ost = bak;
+            this.speed = speed;
+            this.probeg = probeg;
+        }
+
+        public void Out()
+        {
+            Console.WriteLine($"Номер авто: {nom}");
+            Console.WriteLine($"Количество бензина в баке: {ost} л");
+            Console.WriteLine($"Расход топлива на 100 км: {ras:F2} л/100км");
+            Console.WriteLine($"Текущая скорость: {speed:F2} км/ч");
+            Console.WriteLine($"Пробег: {probeg:F2} км");
+        }
+
+        public void Zapravka(double top)
+        {
+            ost += top;
+            Console.WriteLine($"Бак заправлен, в баке находится: {ost}");
+        }
+
+
+        public void Move(int speed)
+        {
+            if (speed <= 0)
+            {
+                Console.WriteLine("Скорость должна быть больше нуля.");
+                return;
+            }
+
+            Console.Write("Введите расстояние: ");
+            double distance = double.Parse(Console.ReadLine());
+
+
+            double kmNalitr = 100/ ras;
+            double rashod = distance * (ras / 100);
+
+
+
+            if (ost >= rashod && ost != 0)
+            {
+                ost -= rashod;
+                probeg += distance;
+                Console.WriteLine($"Проехано {distance:F2} км со скоростью {this.speed}. Остаток топлива: {ost:F2}\nОбщий пробег: {probeg:F2}");
+                this.speed = speed;
+                
+            }
+            else if (ost <= rashod && ost != 0)
+            {
+                double distance1;
+                distance1 = ost * kmNalitr;
+                probeg += distance1;
+                ost = 0;
+                Console.WriteLine($"Проехано {distance1:F2} км со скоростью {this.speed}. Топливо закончилось.\nОбщий пробег: {probeg:F2}");
+                this.speed = speed;
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно топлива для поездки.");
+            }
+        }
+
+        private double Ostatok()
+        {
+            return ost;
+        }
+
+        public void Ostanovka()
+        {
+            Console.WriteLine("Автомобиль остановлен.");
+            speed = 0; 
+        }
+
+        public void Razgon(int sum_speed)
+        {
+            if (ost >= 1.0)
+            {
+                speed += sum_speed;
+                ras += 0.1;
+                ost -= 1.0;
+                Console.WriteLine($"Автомобиль разгоняется до скорости {speed} км/ч. Расход топлива увеличен.");
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно топлива для разгона.");
+            }
+        }
+        public void Tormoz(int sum_speed1)
+        {
+            speed -= sum_speed1; 
+            ras -= 0.1;
+            ost += 1.0;
+            Console.WriteLine($"Автомобиль замедляется до скорости {speed} км/ч. Расход топлива уменьшен.");
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace bebra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество бензина для начальной заправки бака: ");
+            float zalit = float.Parse(Console.ReadLine());
+            Console.Write("Введите начальную скорость для поездки: ");
+            int speed = int.Parse(Console.ReadLine());
+            auto myCar = new auto(zalit, speed);
+            myCar.Info("ABC123", zalit, 8.5f, speed, 0);
+
+            while (true)
+            {
+                Console.WriteLine("\nВыберите действие:");
+                Console.WriteLine("1 - Поехать");
+                Console.WriteLine("2 - Увеличить скорость");
+                Console.WriteLine("3 - Уменьшить скорость");
+                Console.WriteLine("4 - Остановка");
+                Console.WriteLine("5 - Заправить автомобиль");
+                Console.WriteLine("6 - Вывести информацию");
+                Console.WriteLine("7 - Выход\n");
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.D1:
+                        myCar.Move(speed);
+                        break;
+
+                    case ConsoleKey.D2:
+                        Console.Write("Введите скорость для ускорения: ");
+                        int sum_speed = int.Parse(Console.ReadLine());
+                        myCar.Razgon(sum_speed);
+                        break;
+
+                    case ConsoleKey.D3:
+                        Console.Write("Введите скорость для замедления: ");
+                        int sum_speed1 = int.Parse(Console.ReadLine());
+                        myCar.Tormoz(sum_speed1);
+                        break;
+
+                    case ConsoleKey.D4:
+
+                        myCar.Ostanovka();
+                        break;
+
+                    case ConsoleKey.D5:
+                        Console.Write("Введите количество бензина для дозаправки: ");
+                        double top = double.Parse(Console.ReadLine());
+                        myCar.Zapravka(top);
+                        break;
+
+                    case ConsoleKey.D6:
+                        myCar.Out();
+                        break;
+
+                    case ConsoleKey.D7:
+                        Console.WriteLine("Программа завершена.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Неверный выбор. Повторите попытку.");
+                        break;
+                }
+            }
+        }
+    }
+}
